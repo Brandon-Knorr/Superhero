cea052261ecca15f2bae07c21561b9fbfd1bf89c
Brandon-Knorr <bknorr@my.wctc.edu>
2025-11-10 13:22:04 -0600

    feat(created the layout and class of quiz questions)

---
+++ QuizQuestions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero
+{
+    internal class QuizQuestions
+    {
+        public string Text { get; set; }
+
+        public string ImageSource { get; set; }
+
+        public int TrueSpiderman { get; set; }
+
+        public int TrueWolverine { get; set; }
+
+        public int TrueHulk { get; set; }
+
+        public int TrueSuperman { get; set; }
+
+        public int FalseSpiderman { get; set; }
+
+        public int FalseWolverine { get; set; }
+
+        public int FalseHulk { get; set; }
+
+        public int FalseSuperman { get; set; }
+
+        public QuizQuestions(string text, string imageSource, int trueSpiderman, int trueWolverine, int trueHulk, int trueSuperman, int falseSpiderman, int falseWolverine, int falseHulk, int falseSuperman)
+        {
+            Text = text;
+            ImageSource = imageSource;
+            TrueSpiderman = trueSpiderman;
+            TrueWolverine = trueWolverine;
+            TrueHulk = trueHulk;
+            TrueSuperman = trueSuperman;
+            FalseSpiderman = falseSpiderman;
+            FalseWolverine = falseWolverine;
+            FalseHulk = falseHulk;
+            FalseSuperman = falseSuperman;
+        }
+    }
+}
